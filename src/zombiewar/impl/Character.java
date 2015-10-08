f62ea15b349ec71b482459b16c8faef85691bbc1
NickBolles <nickbolles@live.com>
2015-10-08 14:48:54 -0500

    add a dynamic hit amount by adding a getHit() function to the character interface and having the constructor be in the form super(<Health>, <Max Attack>)

--- src/zombiewar/impl/Character.java
+++ src/zombiewar/impl/Character.java
@@ -5,11 +5,18 @@ import zombiewar.intf.ICharacter;
 public abstract class Character implements ICharacter{
 	
 	public int health;
+	public int maxHit;
 	
-	Character(int health){
+	Character(int health, int maxHit){
 		this.health = health;
+		this.maxHit = maxHit;
 	}
-
+        public int getHit(){
+            //restrict the random ammount of the hit between 
+            //50% and 100% of the max hit ammount
+            double percent = Math.min(Math.random() + 0.5, 1);
+            return (int) Math.floor(percent*this.maxHit);
+        }
 	@Override
 	public void decreaseHealth(int increment) {
 		health-=increment;
